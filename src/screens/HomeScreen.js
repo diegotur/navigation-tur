9c2bbbbc7064ff597c75412b84164a819cde3597
diegotur <gpslinea15@hotmail.com>
2023-03-20 15:49:37 -0300

    nav

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Buttons from '../components/Button'
 import Colors from '../constants/Colors'
 import { buttonStyle, buttonTextStyle } from '../constants/Styles'
+import {Ionicons} from '@expo/vector-icons'
 
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
       <Text style={styles.screenText}>RECEPCIÓN Y ENTREGA DE LA UNIDAD DEL CONDUCTOR</Text>
+      <Ionicons name = "md-cart" size={24} ></Ionicons>
       <Buttons styleButtonType={styles.button}
         styleButtonTextType={styles.buttonText}
         title="IR A TOMA DE SERVICIO" onPress={() => {
